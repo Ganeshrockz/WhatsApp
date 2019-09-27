bbfcb5321446f72639b6424c274aaa04e9113134
Sukin Kumar K <kullsno2@gmail.com>
2019-09-27 15:55:56 +0530

    Update WhatsApp.py
    changed some comments

--- WhatsApp.py
+++ WhatsApp.py
@@ -5,8 +5,8 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
  
-# Replace below path with the absolute path of chromedriver in your PC
-driver = webdriver.Chrome('/home/saket/Downloads/chromedriver')
+# Replace the path specified below with the absolute path to chromedriver in your PC
+driver = webdriver.Chrome('/PATH/TO/CHROMEDRIVER')
  
 driver.get("https://web.whatsapp.com/")
 wait = WebDriverWait(driver, 600)
@@ -24,9 +24,9 @@ group = wait.until(EC.presence_of_element_located((
     By.XPATH, arg)))
 group.click()
 inp = '//div[@class="input"][@dir="auto"][@data-tab="1"]'
-input1 = wait.until(EC.presence_of_element_located((
+inputAction = wait.until(EC.presence_of_element_located((
     By.XPATH, inp)))
 for i in range(100):
  # Sends the Message
-    input1.send_keys(string + Keys.ENTER)
+    inputAction.send_keys(string + Keys.ENTER)
     time.sleep(1)
